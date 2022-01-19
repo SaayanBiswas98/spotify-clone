71366422abf9f6787cf14afc38f365df5f7b09ab
SaayanBiswas98 <76898125+SaayanBiswas98@users.noreply.github.com>
2022-01-19 10:10:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="Header__left">
+          <h2>
+            Songs
+            New
+            Premium
+            Download
+          </h2>
+          </div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg"alt=" "/>
+      </div>
+      <div className="Main">
+        <img src="https://cdn.mos.cms.futurecdn.net/r6hTivnwioG2GbKP33oiqk.jpg"/>
+      <div>
+        <div className='button'>
+        <button className='btn'>SIGNUP </button>
+        </div>
+      
+        </div> 
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
